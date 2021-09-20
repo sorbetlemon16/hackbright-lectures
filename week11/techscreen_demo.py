1c52b962abee9df6b0d9e8c58653e8bf533c80be
Jocelyn Tang <jocelyn.tang@hackbrightacademy.com>
2021-09-20 22:21:24 +0000

    recursion tech screen demo

---
+++ week11/techscreen_demo.py
@@ -0,0 +1,20 @@
+# using recursion, print everything in a list
+
+# print an item
+# recursive call to the function th
+
+####################
+
+# function: input a list
+# check for base case: return when the list is empty
+# print an item
+# pop that item off the list
+# recursive call: input of shortened list (popping off last element)
+
+# other things to consider: list is destroyed vs. slicing, base case being different
+
+def print_recursively():
+    print("Hello!")
+
+if __name__ == "__main__":
+    print_recursively()
